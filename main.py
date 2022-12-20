5c127a7c2ace10b5ff4e1ccf7e227f7e7e188b40
Nazmul Hridoy <nazmul.hridoy007@gmail.com>
2022-12-20 10:56:30 +0600

    Update main.py

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ while True:
     res = re.content
 
     soup = BeautifulSoup(res, 'html.parser')
-    price = float(soup.find('p', class_='price_color').text[1:])
+    price = float(soup.find('p', class_='price_color').text[1:])    # float is using here to set price value as a floating number.
 
-    # Checking if the price is less than 40
-    # Use your email and the password (you can generate a password for the app from your Yahoo account)
+    # Checking if the price is less than 60
+    # Use your email and the password (you can generate a password for the app from your gmail account)
 
     if price < 60:
         smt = smtplib.SMTP('smtp.gmail.com', 587)
@@ -24,10 +24,13 @@ while True:
 
         smt.starttls()
         # Use your credentials
-        smt.login('nazmul.hridoy007@gmail.com', 'tctmwokcpsxmjlrd')
-        # First email is the sender's email, the second is the receiver's email
-        smt.sendmail('nazmul.hridoy007@gmail.com',
-                     'nazmul3224@diu.edu.bd',
+        # STARTTLS notifies a mail server that the contents of an email need to be encrypted
+        smt.login('YourEmail@gmail.com', 'YourPassword')
+        
+       
+        smt.sendmail('SenderEmail@gmail.com',
+                     'ReceiverEmail@gmail.com',
                      f"Subject: Books Price Notifier\n\nHi, price has dropped to {price}. Buy it!")
+         # First email is the sender's email aka YourEmail@gmail.com, the second is the receiver's email
         smt.quit()
-    time.sleep(24 * 60 * 60)
+    time.sleep(24 * 60 * 60) # It'll check the price one time in each day
